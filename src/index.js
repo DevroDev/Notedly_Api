9a822814db8db8bab7754121379cc63efdc7730c
Devro <mac@MACs-MacBook-Pro.local>
2021-05-01 22:38:03 +0630

    Notedly

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { gql, ApolloServer } = require('apollo-server-express');
+
+require('dotenv').config();
+
+const typeDefs=require('./schema');
+const resolvers=require('./resolvers')
+
+const db = require('./db');
+const models = require('./models');
+
+const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+
+const app = express();
+db.connect(DB_HOST);
+
+const server = new ApolloServer({ typeDefs, resolvers,context:()=>{
+  return {models}
+} });
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen(port, () => console.log('Listening on port 4000'));
